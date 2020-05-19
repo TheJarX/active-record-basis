e2912bb26db3c0c16df52023ad5936c26d4330a2
Gerard Altamirano <galtamiranor@outlook.es>
2020-05-19 14:49:32 -0500

    Add validations to the Game table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  validates :name, :genre, :price, :release_date, :company_id, presence: true
+  validates :genre, inclusion: { in: ['Simulator', 'Adventure', 'Strategy', 'Role-playing (RPG)', 'Shooter', 'Fighting', 'Sport']}
+  validates :price, numericality: { greater_than: 0 }
 end
